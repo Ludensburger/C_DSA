d3e6ed9b005a4ebebcc74ab5e82957d1a24bcbd9
Ludensburger <mendozaryu08@gmail.com>
2024-04-06 05:58:41 +0800

    refactor: added documentation

--- DynamicArray/main.cpp
+++ DynamicArray/main.cpp
@@ -2,36 +2,56 @@
 // Date: April 6, 2024
 // Description: A program that demonstrates the implementation of a dynamic array list.
 
+/*
+ * This function creates a DynamicArrayList object and provides a menu for the user to interact with the list.
+ * The user can perform the following operations:
+ *
+ * 'a': Add an element to the list. The user must provide the element to be added after the 'a' command.
+ * 'r': Remove an element from the list. The user must provide the element to be removed after the 'r' command.
+ * 'p': Print the list.
+ * 'x': Exit the program.
+ */
+
 #include "myArrayList.h"
 #include <iostream>
 using namespace std;
 
 int main() {
+    // Create a DynamicArrayList object
     DynamicArrayList myList;
+
     char op;
     int num;
 
     do {
+
         cout << "Op: ";
         cin >> op;
 
         switch (op) {
+
+            // Sample Input: a10
         case 'a':
 
             cin >> num;
             myList.add(num);
             break;
+
+            // Sample Input: r10
         case 'r':
 
             cin >> num;
             myList.remove(num);
             break;
+
         case 'p':
             myList.print();
             break;
+
         case 'x':
             cout << "Exiting program." << endl;
             break;
+
         default:
             cout << "Invalid operation." << endl;
             break;
