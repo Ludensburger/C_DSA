1d5be7961567b7b9f8f3d246e65ad318b1b0164c
Ludensburger <mendozaryu08@gmail.com>
2024-04-07 00:23:03 +0800

    refactor: Add driver.cpp file with main menu and program execution logic for all the Data Structures

---
+++ driver.cpp
@@ -0,0 +1,56 @@
+#include <cstdlib> // For system("cls") to clear the screen
+#include <iostream>
+using namespace std;
+
+int main() {
+    int choice;
+
+    do {
+        // Display the main menu
+        cout << "Main Menu" << endl;
+        cout << "1. Dynamic Array List" << endl;
+        cout << "2. Singly Linked List" << endl;
+        cout << "3. Doubly Linked List" << endl;
+        cout << "4. Exit" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        // Clear the screen
+        system("cls");
+
+        // Based on user's choice, execute the corresponding program
+        switch (choice) {
+        case 1:
+            cout << "Running Dynamic Array List program..." << endl;
+            // Code to run the Dynamic Array List program
+            system("\"C:\\Users\\hp\\Desktop\\Ryu Files\\Codes\\C-Programming\\CPP\\C_DataStructures\\DynamicArray\\main.exe\"");
+            break;
+        case 2:
+            cout << "Running Singly Linked List program..." << endl;
+            // Code to run the Singly Linked List program
+            system("\"C:\\Users\\hp\\Desktop\\Ryu Files\\Codes\\C-Programming\\CPP\\C_DataStructures\\SinglyLinkedList\\main.exe\"");
+            break;
+        case 3:
+            cout << "Running Doubly Linked List program..." << endl;
+            // Code to run the Doubly Linked List program
+            system("\"C:\\Users\\hp\\Desktop\\Ryu Files\\Codes\\C-Programming\\CPP\\C_DataStructures\\DoublyLinkedList\\main.exe\"");
+            break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please enter a valid option." << endl;
+        }
+
+        // Wait for user to press Enter before clearing the screen and displaying the menu again
+        cout << "Press Enter to continue...";
+        cin.ignore(); // Ignore any previous newline characters in the input buffer
+        cin.get();    // Wait for user to press Enter
+
+        // Clear the screen before displaying the menu again
+        system("cls");
+
+    } while (choice != 4);
+
+    return 0;
+}
