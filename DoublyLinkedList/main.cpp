65fadc345b9a6906cb7af7bf47a6561aca82d981
Ludensburger <mendozaryu08@gmail.com>
2024-04-07 00:28:55 +0800

    refactor: Added main.cpp with main menu options and user input prompt

--- DoublyLinkedList/main.cpp
+++ DoublyLinkedList/main.cpp
@@ -9,6 +9,17 @@ int main() {
     int num, pos;
     char op;
 
+    cout << "Menu:" << endl;
+    cout << "h: Add an element to the beginning of the list." << endl;
+    cout << "t: Add an element to the end of the list." << endl;
+    cout << "@: Add an element at a specific position in the list." << endl;
+    cout << "r: Remove an element from the list by value." << endl;
+    cout << "F: Remove the first element from the list." << endl;
+    cout << "L: Remove the last element from the list." << endl;
+    cout << "g: Get an element from a specific position in the list." << endl;
+    cout << "p: Print the list." << endl;
+    cout << "x: Exit the program." << endl;
+
     do {
         cout << "Op: ";
         cin >> op;
