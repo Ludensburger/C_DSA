079f0a2b22894c854510761066d6048bc19a5d76
Ludensburger <mendozaryu08@gmail.com>
2024-04-06 23:54:17 +0800

    refactor: main.cpp to use DoublyLinkedList instead of LinkedList

---
+++ DoublyLinkedList/main.cpp
@@ -0,0 +1,57 @@
+// Author: Ryu R. Mendoza
+// Date: April 6, 2024
+
+#include "linkedlist.h"
+#include <iostream>
+
+int main() {
+    LinkedList *myDList = new LinkedList();
+    int num;
+    int pos;
+    char op;
+
+    do {
+        cout << "Op: ";
+        cin >> op;
+
+        switch (op) {
+
+        case 'h':
+            cin >> num;
+            myDList->addFirst(num);
+            break;
+        case 't':
+            cin >> num;
+            myDList->addLast(num);
+            break;
+        case '@':
+            cin >> num >> pos;
+            myDList->addAt(num, pos);
+            break;
+        case 'r':
+            cin >> num;
+            cout << "Remove pos " << myDList->remove(num) << endl;
+            break;
+        case 'F':
+            myDList->removeFirst();
+            break;
+        case 'L':
+            myDList->removeLast();
+            break;
+        case 'g':
+            cin >> num;
+            cout << myDList->get(num) << endl;
+            break;
+        case 'p':
+            myDList->print();
+            break;
+        case 'x':
+            cout << "Exiting" << endl;
+            break;
+        }
+    } while (op != 'x');
+
+    delete myDList; // Don't forget to deallocate memory
+
+    return 0;
+}
