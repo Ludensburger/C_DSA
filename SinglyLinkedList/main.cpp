a9079d7edad300475008cd54c6d639b572e94b5a
Ludensburger <mendozaryu08@gmail.com>
2024-04-06 21:51:06 +0800

    refactor: added main menu for SinglyLinkedList to use LinkedList class

---
+++ SinglyLinkedList/main.cpp
@@ -0,0 +1,67 @@
+#include "linkedlist.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Create a DynamicArrayList object
+    LinkedList *myList;
+
+    char op;
+    int num;
+    int pos;
+
+    do {
+
+        cout << "Op: ";
+        cin >> op;
+
+        switch (op) {
+
+            // Add a new node to the head (farthest left)
+            // Sample Input: h10
+        case 'h':
+
+            cin >> num;
+            myList->addHead(num);
+            break;
+
+            // Add a new node to the tail (farthest right)
+            // Sample Input: h10
+        case 't':
+
+            cin >> num;
+            myList->addTail(num);
+            break;
+
+            // Remove a node from the list
+            // Sample Input: h20
+        case 'r':
+
+            cin >> num;
+            cout << "Remove Position " << myList->remove(num) << endl;
+            break;
+
+        case 'g':
+
+            cin >> pos;
+            myList->get(pos);
+            break;
+
+            // Print the list
+        case 'p':
+            myList->print();
+            break;
+
+            // Exit the program
+        case 'x':
+            cout << "Exiting program." << endl;
+            break;
+
+        default:
+            cout << "Invalid operation." << endl;
+            break;
+        }
+    } while (op != 'x');
+
+    return 0;
+}
