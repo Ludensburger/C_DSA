1c8484afb36a9b5ecd78a791d1a1ea64bf88b91a
Ludensburger <147230279+Ludensburger@users.noreply.github.com>
2024-04-07 17:22:01 +0800

    refactor: Add Stack interface definition

---
+++ Stack/stack.h
@@ -0,0 +1,10 @@
+class Stack
+{
+public:
+    virtual void push(int) = 0;
+    virtual int pop() = 0;
+    virtual void print() = 0;
+    virtual int isEmpty() = 0;
+    virtual int size() = 0;
+    virtual int peek() = 0;
+};
