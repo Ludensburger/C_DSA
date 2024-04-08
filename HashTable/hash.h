e23421c3cf0bd53d86febe6f7062611d37e6ecb8
Ludensburger <mendozaryu08@gmail.com>
2024-04-08 11:48:04 +0800

    refactor: Hash class interface in hash.h

--- HashTable/hash.h
+++ HashTable/hash.h
@@ -1,10 +1,10 @@
-class Hash
-{
+class Hash {
 public:
     virtual void insert(int key, int value) = 0;
-    // virtual int remove(int key) = 0;
-    // virtual int search(int key) = 0;
-    // virtual void resize(int newSize) = 0;
-    // virtual int hash(int key) = 0;
+    virtual int remove(int key, int value) = 0;
+    virtual void get(int key) = 0;
+    virtual void resize(int newSize) = 0;
+    virtual int hash(int key) = 0;
+    virtual int _size() = 0;
     virtual void print() = 0;
 };
